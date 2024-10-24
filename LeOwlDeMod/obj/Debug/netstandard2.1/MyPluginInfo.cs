e17248ac30a1172fec995d2fc031aea074b5c67e
GamerOwls <laurens.vanderheiden@student.hku.nl>
2024-10-25 01:17:20 +0200

    Initial Commit

---
+++ LeOwlDeMod/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LeOwlDeMod
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "LeOwlDeMod";
+        public const string PLUGIN_NAME = "LeOwlDeMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
